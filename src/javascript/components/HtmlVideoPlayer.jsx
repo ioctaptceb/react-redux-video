db445995a5b0e524481cb8b7d1edfbe08975de76
ioctaptceb <yulia.startsev@gmail.com>
2016-10-30 18:03:03 +0100

    add play/pause to Html player

--- src/javascript/components/HtmlVideoPlayer.jsx
+++ src/javascript/components/HtmlVideoPlayer.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
-const HtmlVideoPlayer = ({videoUrl}) => {
-  return (
-    <div>
-      HTML Player
-      <video src={videoUrl}>
-      </video>
-    </div>
-  );
-};
+class HtmlVideoPlayer extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentWillUpdate({playState}) {
+    switch(playState) {
+    case 'play':
+      return this.player.play();
+    case 'pause':
+      return this.player.pause();
+    default:
+      return;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        HTML Player
+        <video ref={(node) => {this.player = node;}} src={this.props.videoUrl}>
+        </video>
+      </div>
+    );
+  }
+}
 
 export default HtmlVideoPlayer;
 
--- src/javascript/components/HtmlVideoPlayer.spec.jsx
+++ src/javascript/components/HtmlVideoPlayer.spec.jsx
@@ -3,8 +3,29 @@ import {shallow} from 'enzyme';
 import HtmlVideoPlayer from './HtmlVideoPlayer.jsx';
 
 it('contains a video element', () => {
-  const sampleVideo = 'http://something.com/some-video'
+  const sampleVideo = 'http://something.com/some-video';
   const videoPlayer = shallow(<HtmlVideoPlayer videoUrl={sampleVideo}/>);
 
   expect(videoPlayer.find('video').props().src).toBe(sampleVideo);
 });
+
+it('plays a video when playState changes to play', () => {
+  const sampleVideo = 'http://something.com/some-video';
+  const videoPlayer = shallow(<HtmlVideoPlayer videoUrl={sampleVideo}/>);
+  const instance = videoPlayer.instance();
+  instance.player = jasmine.createSpyObj('player', ['play', 'pause']);
+  videoPlayer.setProps({playState: 'play'});
+
+  expect(instance.player.play).toHaveBeenCalled();
+});
+
+it('pauses a video when playState changes to pause', () => {
+  const sampleVideo = 'http://something.com/some-video';
+  const videoPlayer = shallow(<HtmlVideoPlayer videoUrl={sampleVideo}/>);
+  const instance = videoPlayer.instance();
+  instance.player = jasmine.createSpyObj('player', ['play', 'pause']);
+  videoPlayer.setProps({playState: 'pause'});
+
+  expect(instance.player.pause).toHaveBeenCalled();
+});
+
